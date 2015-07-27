01e2115b4d3223ba8368caf0b766e28d23fe3e9f
KT-Boundary <kmal2615@DS02.libstaff.library.usyd.edu.au>
2015-07-27 12:29:00 +1000

    Added a button to feed the cat and lines in the js file to make the image wider after feeding

--- interaction.js
+++ interaction.js
@@ -1,5 +1,7 @@
-var cat_image = document.getElementByID("cat");
-cat_image.addEventListener("click", function() {
-  alert("Meow!");
-  sleep();
-});
+var cat_image = document.getElementByID('cat');
+var feed_button = document.getElementByID('feed_button')
+
+feed_button.addEventListener("click", feed);
+function feed() {
+  cat_image.style.width = (cat_image.offsetWidth + 30.0) + 'px';
+};
